a4b16656914f37319849d2d373b1fceefe75c21f
k3nt1k2 <Nikis007575@gmail.com>
2024-11-28 19:20:20 +0900

    create_prj

---
+++ app/src/main/java/com/example/lab/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lab
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn : Button = binding.btn1
+
+        btn.setOnClickListener{
+            intent = Intent(this,MainActivity2::class.java)
+            intent.putExtra("text","яичко228")
+            startActivity(intent)
+        }
+    }
+}
